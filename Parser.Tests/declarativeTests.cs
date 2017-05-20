0f404015fac84a07de8fc38eba072879c5a3af66
kenystev <kjestevez.0795@gmail.com>
2017-05-20 05:11:49 -0600

    interface declaration is done

--- Parser.Tests/declarativeTests.cs
+++ Parser.Tests/declarativeTests.cs
@@ -9,6 +9,7 @@ namespace Parser.Tests
         [Fact]
         public void usingStmtTest()
         {
+            printHeader("usingStmtTest");
             var inputString = new InputFile(@"..\..\..\testFiles\usingTests.txt");
             var tokenGenerators = Resources.getTokenGenerators();
 
@@ -21,6 +22,7 @@ namespace Parser.Tests
         [Fact]
         public void namespacesStmtTest()
         {
+            printHeader("namespacesStmtTest");
             var inputString = new InputFile(@"..\..\..\testFiles\namespaceTests.txt");
             var tokenGenerators = Resources.getTokenGenerators();
 
@@ -33,6 +35,7 @@ namespace Parser.Tests
         [Fact]
         public void classDeclarationStmtTest()
         {
+            printHeader("classDeclarationStmtTest");
             var inputString = new InputFile(@"..\..\..\testFiles\classDeclarationTests.txt");
             var tokenGenerators = Resources.getTokenGenerators();
 
@@ -45,6 +48,7 @@ namespace Parser.Tests
         [Fact]
         public void enumDeclarationStmtTest()
         {
+            printHeader("enumDeclarationStmtTest");
             var inputString = new InputFile(@"..\..\..\testFiles\enumDeclarationTests.txt");
             var tokenGenerators = Resources.getTokenGenerators();
 
@@ -57,6 +61,7 @@ namespace Parser.Tests
         [Fact]
         public void interfaceDeclarationStmtTest()
         {
+            printHeader("interfaceDeclarationStmtTest");
             var inputString = new InputFile(@"..\..\..\testFiles\interfaceDeclarationTests.txt");
             var tokenGenerators = Resources.getTokenGenerators();
 
@@ -64,6 +69,13 @@ namespace Parser.Tests
             var parser = new Compiler.Parser(lexer); 
             parser.parse();
             Console.Out.WriteLine("Parsed Successfully -> enum");
+        }
+
+        private void printHeader(string v)
+        {
+            Console.Out.WriteLine("----------------------------------------");
+            Console.Out.WriteLine(v);
+            Console.Out.WriteLine("----------------------------------------");
         }
     }
 }
